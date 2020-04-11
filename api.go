3bbeebfb5104854b7c4d948f26a8b4ddbe8c43e8
Christopher Hayles <chrishaylesuk@outlook.com>
2020-04-11 15:08:49 -0700

    removed test server

--- api.go
+++ api.go
@@ -4,14 +4,9 @@ import (
 	"net/http"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// DefaultEndpoint returns content for ./
+func DefaultEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "hello world"}`))
-}
-
-func NewServer() *server {
-	return &server{}
 }
