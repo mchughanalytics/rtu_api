23ef3e0d44f6de0442c77695f7c8fed8d7a32c95
Christopher Hayles <chrishaylesuk@outlook.com>
2020-04-11 14:49:17 -0700

    server added

---
+++ api.go
@@ -0,0 +1,13 @@
+package rtuapi
+
+import (
+	"net/http"
+)
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
